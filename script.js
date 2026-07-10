b37614cecc662cc6b28b268a7640e14f5dc56ce7
Emily <emilylin080807@gmail.com>
2026-07-10 01:49:54 -0500

    Added access to HTML elements

---
+++ script.js
@@ -0,0 +1,3 @@
+const scaleSelect = document.getElementById('scaleSelect');
+const startButton = document.getElementById('startButton');
+const scaleDisplay = document.getElementById('scaleDisplay');
