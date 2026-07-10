4c92084b049af1ecd80c1e828838f627078543b5
Emily <emilylin080807@gmail.com>
2026-07-10 01:57:03 -0500

    Added basic functionality to the start button

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const scaleSelect = document.getElementById('scaleSelect');
 const startButton = document.getElementById('startButton');
 const scaleDisplay = document.getElementById('scaleDisplay');
+
+console.log("Scale Trainer loaded");
+
+startButton.addEventListener('click', function() {
+    console.log("Start button clicked");
+});
