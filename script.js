72159c4abc5a768b309aeee073993847f3d176f8
Emily <emilylin080807@gmail.com>
2026-07-10 02:09:13 -0500

    Gets the selected scale from the dropdown menu

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const scaleSelect = document.getElementById('scaleSelect');
 const startButton = document.getElementById('startButton');
 const scaleDisplay = document.getElementById('scaleDisplay');
 
-console.log("Scale Trainer loaded");
+console.log("Scale Trainer loaded"); // Debugging statement to make sure JavaScript is connected to the HTML file
 
 startButton.addEventListener('click', function() {
-    console.log("Start button clicked");
+    //console.log("Start button clicked"); // Debugging statement to make sure the button click is being detected
+    const selectedScale = scaleSelect.value;
+    console.log(selectedScale);
 });
