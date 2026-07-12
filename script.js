5412b4658fd8de1b8e2ec9a3f54fd4597d55b35b
Emily <emilylin080807@gmail.com>
2026-07-12 02:18:51 -0500

    Updated so the notes of the selected major will appear on the screen

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ const scales = {
     "A Major": ["A", "B", "C#", "D", "E", "F#", "G#", "A"],
     "B Major": ["B", "C#", "D#", "E", "F#", "G#", "A#", "B"]
 };
-console.log(scales["D Major"]);
+console.log(scales["C Major"]); // Making sure the scales object is being logged correctly
 
 startButton.addEventListener('click', function() {
     //console.log("Start button clicked"); // Debugging statement to make sure the button click is being detected
     const selectedScale = scaleSelect.value;
+    const notes = scales[selectedScale];
     //console.log(selectedScale); // Making sure the selected scale is being logged correctly
-    scaleDisplay.innerHTML = "Now practicing: " + selectedScale;
+    scaleDisplay.innerHTML = "Now practicing: " + selectedScale + "<br>Notes: " + notes.join(" ");
 });
