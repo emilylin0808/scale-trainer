df25b3c993309bb7297be8af920059874f8fffb0
Emily <emilylin080807@gmail.com>
2026-07-14 23:44:23 -0500

    Added in microphone accses

--- script.js
+++ script.js
@@ -16,11 +16,14 @@ const scales = {
 };
 console.log(scales["C Major"]); // Making sure the scales object is being logged correctly
 
+// ----------------------------------------------Variables------------------------------------------------------------------
 let practicing = false;
 let seconds = 0;
 let timer;
 let currentScale = scaleSelect.value;
+let practiceHistory = []; // Array to store previous practice data
 
+// ----------------------------------------------Event Listeners------------------------------------------------------------------
 startButton.addEventListener('click', function() { // Starts or stops the practice session when the button is clicked
     //console.log("Start button clicked"); // Debugging statement to make sure the button click is being detected
     currentScale = scaleSelect.value;
@@ -34,6 +37,7 @@ startButton.addEventListener('click', function() { // Starts or stops the practi
 });
 
 scaleSelect.addEventListener("change", updateScaleDisplay); // Updates the scale display when a new scale is selected
+
 // ----------------------------------------------Functions------------------------------------------------------------------
 function startPractice() {
     practicing = true;
@@ -53,6 +57,8 @@ function stopPractice() {
     startButton.innerHTML = "Start Practice";
     status.innerHTML = "Practice finished.";
     clearInterval(timer);
+    practiceHistory.push({ scale: currentScale, time: seconds }); // Store the practice data
+    console.log("Practice history: ", practiceHistory); // Log the practice history
 }
 
 function updateScaleDisplay() { // Updates the scale display based on the selected scale
@@ -61,4 +67,8 @@ function updateScaleDisplay() { // Updates the scale display based on the select
     scaleDisplay.innerHTML = " <strong>" + currentScale + "</strong><br>Notes: " + notes.join(" ");
 }
 
+async function requestMicrophone() { // Requests microphone access from the user and waits until the user grants or denies access
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    console.log(stream); // Log the stream to make sure the microphone access is working
+}
 updateScaleDisplay(); // Initial call to display the default scale when the page loads
